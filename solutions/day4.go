609ff8197414dc5e3c9a9c48da058125d21a79e7
justjcurtis <justjcurtis@gmail.com>
2024-12-04 14:30:41 +0000

    perf: further optimise day 4

--- solutions/day4.go
+++ solutions/day4.go
@@ -2,11 +2,10 @@ package solutions
 
 import (
 	"AdventOfCode2024/utils"
+	"math"
 	"strconv"
-	"strings"
 )
 
-var noDirection = []int{0, 0}
 var up = []int{0, -1}
 var down = []int{0, 1}
 var left = []int{-1, 0}
@@ -17,66 +16,66 @@ var downLeft = []int{-1, 1}
 var downRight = []int{1, 1}
 var directions = [][]int{up, down, left, right, upLeft, upRight, downLeft, downRight}
 
-func getNeighbors(x, y, n int, d []int, input []string) []string {
+func getNeighborsRunner(x, y, n int, d []int, input []string) byte {
 	if n == 0 {
-		return []string{}
+		return 0
 	}
 	if x < 0 || x >= len(input[0]) || y < 0 || y >= len(input) {
-		return []string{}
+		return 0
 	}
-	if d[0] == 0 && d[1] == 0 {
-		neighbors := []string{}
-		for _, dir := range directions {
-			next := append([]string{string(input[y][x])}, getNeighbors(x+dir[0], y+dir[1], n-1, dir, input)...)
-			neighbors = append(neighbors, strings.Join(next, ""))
+	curr := input[y][x] * byte(math.Pow(2, float64(n-1)))
+	return curr + getNeighborsRunner(x+d[0], y+d[1], n-1, d, input)
+}
+
+func getNeighbors(x, y, n int, input []string) int {
+	xmasCount := 0
+	curr := input[y][x] * byte(math.Pow(2, float64(n-1)))
+	for _, dir := range directions {
+		if int(curr+getNeighborsRunner(x+dir[0], y+dir[1], n-1, dir, input)) == 201 {
+			xmasCount++
 		}
-		filtered := []string{}
-		for _, neighbor := range neighbors {
-			if len(neighbor) == n {
-				filtered = append(filtered, neighbor)
-			}
-		}
-		return filtered
 	}
-	return append([]string{string(input[y][x])}, getNeighbors(x+d[0], y+d[1], n-1, d, input)...)
+	return xmasCount
 }
 
 func solveDay4Part1(input []string) int {
 	fn := func(z int) int {
-		xmasCount := 0
 		x, y := utils.OneDTwoD(z, len(input[0]))
 		char := input[y][x]
 		if char == 'X' {
-			neighbors := getNeighbors(x, y, 4, noDirection, input)
-			for _, neighbor := range neighbors {
-				if neighbor == "XMAS" {
-					xmasCount++
-				}
-			}
+			return getNeighbors(x, y, 4, input)
 		}
-		return xmasCount
+		return 0
 	}
 	return utils.Parallelise(utils.IntAcc, fn, len(input)*len(input[0]))
 }
 
-func getCross(x, y int, input []string) []string {
-	cross := []byte{}
-	offsets := [][]int{upLeft, upRight, downLeft, downRight}
-	for _, offset := range offsets {
-		X := x + offset[0]
-		Y := y + offset[1]
-		if X < 0 || X >= len(input[0]) || Y < 0 || Y >= len(input) {
-			return []string{}
-		}
-		char := input[Y][X]
-		if char != 'M' && char != 'S' {
-			return []string{}
-		}
-		cross = append(cross, char)
+func getByte(coords [][]int, input []string) byte {
+	result := byte(0)
+	for _, coord := range coords {
+		x, y := coord[0], coord[1]
+		result += input[y][x]
+	}
+	return result
+}
+
+func addCoords(x, y int, b []int) []int {
+	return []int{x + b[0], y + b[1]}
+}
+
+func getCross(x, y int, input []string) int {
+	if x < 1 || x >= len(input[0])-1 || y < 1 || y >= len(input)-1 {
+		return 0
 	}
 	curr := input[y][x]
-	strs := []string{string(cross[0] + curr + cross[3]), string(cross[1] + curr + cross[2])}
-	return strs
+	aoffsets := [][]int{addCoords(x, y, upLeft), addCoords(x, y, downRight)}
+	a := int(curr + getByte(aoffsets, input))
+	boffsets := [][]int{addCoords(x, y, upRight), addCoords(x, y, downLeft)}
+	b := int(curr + getByte(boffsets, input))
+	if a == 225 && b == 225 {
+		return 1
+	}
+	return 0
 }
 
 func solveDay4Part2(input []string) int {
@@ -84,13 +83,7 @@ func solveDay4Part2(input []string) int {
 		x, y := utils.OneDTwoD(z, len(input[0]))
 		char := input[y][x]
 		if char == 'A' {
-			crosses := getCross(x, y, input)
-			if len(crosses) == 0 {
-				return 0
-			}
-			if crosses[0] == "á" && crosses[1] == "á" {
-				return 1
-			}
+			return getCross(x, y, input)
 		}
 		return 0
 	}
